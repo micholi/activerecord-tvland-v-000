cae13e6498c24bf6d45ed2450dba094b14a8e04b
Michelle Olivieri <michelleo72@gmail.com>
2018-04-30 22:01:14 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,8 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.each do |character|
-      character.name
+    self.characters.each do |c|
+      character = c.name
     end
   end
 
