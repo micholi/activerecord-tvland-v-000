2ded25693f8967d7056a47c841efd528531e4be3
Michelle Olivieri <michelleo72@gmail.com>
2018-04-30 21:43:41 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,9 +1,10 @@
-class CreateActors < ActiveRecord::Migration[4.2]
-  
+class CreateCharacters < ActiveRecord::Migration[4.2]
+
   def change
-    create_table :actors do |t|
-      t.string :first_name
-      t.string :last_name
+    create_table :characters do |t|
+      t.string :name
+      t.integer :show_id
+      t.integer :actor_id
     end
   end
 
